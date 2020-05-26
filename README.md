d83cd7abdb9d2b3626b0083dc401945277bb0097
cw417 <chriswhitlock417@gmail.com>
2020-05-26 01:07:24 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # NvidiaStats
 Nvidia GPU stat reporting app for Linux
+
+## Table of Contents
+1. About
+2. Getting Started
+    1. Prerequisites
+    2. Installing
+3. How It Works
+
+## About
+NvidiaStats is a command line GPU stat reporting application written for use in a Linux-based operating system running the Xorg display server with proprietary Nvidia drivers. Currently, it will report GPU utilization, GPU clock, GPU temperature, and fan speed.
+
+## Getting Started
+### Prerequisites
+- Python 3.6+
+- Linux-based operating system
+    - Xorg display server
+- Nvidia GPU running proprietary nvidia drivers
+
+### Installing
+1) Clone the repository: `git clone https://github.com/cw417/NvidiaStats`
+2) Ensure you are using prorietary drivers compatible with the NvidiaStats:
+    - `cat xorg.conf | grep nvidia-xconfig`
+    - If this returns an output, you are using proprietary Nvidia drivers
+3) Run NvidiaStats: 
+    - Change directory into the cloned NvidiaStats directory: `cd NvidiaStats`
+    - Run the app: `python app.py`
+
+## How It Works
+    - GPU stats are obtained via the subprocess module running the `nvidia-smi -q` command with the specified stat output option
+    - Output from the subprocess module is parsed via regex
+    - The regex match output is formatted and printed to the command line
---
+++ app.py
@@ -0,0 +1,45 @@
+# WORKFLOW:
+#     - specify command to get GPU stats
+#     - run command with subprocess.check_output
+#     - parse output with regex
+#   TODO:
+#     - output regex match to csv file
+#     - convert csv to real time graph with matplotlib
+
+
+import subprocess
+import re
+
+def get_GPU_temp():
+    cmd = 'nvidia-smi --query-gpu=temperature.gpu --format=csv'
+    check = str(subprocess.check_output(cmd, shell=True))
+    reg = re.compile(r'\d\d')
+    match  = reg.search(check).group(0)
+    print(f'GPU Temp: {match}')
+
+def get_GPU_utilization():
+    cmd = 'nvidia-smi --query-gpu=utilization.gpu --format=csv'
+    check = str(subprocess.check_output(cmd, shell=True))
+    reg = re.compile(r'\d* %')
+    match  = reg.search(check).group(0)
+    print(f'GPU Use: {match}')
+
+def get_GPU_clock():
+    cmd = 'nvidia-smi --query-gpu=clocks.current.graphics --format=csv'
+    check = str(subprocess.check_output(cmd, shell=True))
+    reg = re.compile(r'\d\d\d\d MHz')
+    match  = reg.search(check).group(0)
+    print(f'GPU Clock: {match}')
+
+def get_fan_speed():
+    cmd = 'nvidia-smi --query-gpu=fan.speed --format=csv'
+    check = str(subprocess.check_output(cmd, shell=True))
+    reg = re.compile(r'\d* %')
+    match  = reg.search(check).group(0)
+    print(f'Fan Speed: {match}')
+
+if __name__ == '__main__':
+    get_GPU_clock()
+    get_GPU_utilization()
+    get_fan_speed()
+    get_GPU_temp()
