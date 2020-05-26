a7ceb925b83c6f8fe48bd6f859a5921146d64ead
cw417 <chriswhitlock417@gmail.com>
2020-05-26 01:20:55 -0400

    Added VRAM clock and utilization

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_GPU_temp():
     check = str(subprocess.check_output(cmd, shell=True))
     reg = re.compile(r'\d\d')
     match  = reg.search(check).group(0)
-    print(f'GPU Temp: {match}')
+    print(f'GPU Temp: {match} C')
 
 def get_GPU_utilization():
     cmd = 'nvidia-smi --query-gpu=utilization.gpu --format=csv'
@@ -25,11 +25,25 @@ def get_GPU_utilization():
     print(f'GPU Use: {match}')
 
 def get_GPU_clock():
-    cmd = 'nvidia-smi --query-gpu=clocks.current.graphics --format=csv'
+    cmd = 'nvidia-smi --query-gpu=clocks.gr --format=csv'
     check = str(subprocess.check_output(cmd, shell=True))
     reg = re.compile(r'\d\d\d\d MHz')
     match  = reg.search(check).group(0)
     print(f'GPU Clock: {match}')
+
+def get_VRAM_utilization():
+    cmd = 'nvidia-smi --query-gpu=utilization.memory --format=csv'
+    check = str(subprocess.check_output(cmd, shell=True))
+    reg = re.compile(r'\d* %')
+    match  = reg.search(check).group(0)
+    print(f'VRAM Use: {match}')
+
+def get_VRAM_clock():
+    cmd = 'nvidia-smi --query-gpu=clocks.mem --format=csv'
+    check = str(subprocess.check_output(cmd, shell=True))
+    reg = re.compile(r'\d\d\d\d MHz')
+    match  = reg.search(check).group(0)
+    print(f'VRAM Clock: {match}')
 
 def get_fan_speed():
     cmd = 'nvidia-smi --query-gpu=fan.speed --format=csv'
@@ -39,7 +53,9 @@ def get_fan_speed():
     print(f'Fan Speed: {match}')
 
 if __name__ == '__main__':
-    get_GPU_clock()
     get_GPU_utilization()
+    get_GPU_clock()
+    get_VRAM_utilization()
+    get_VRAM_clock()
     get_fan_speed()
     get_GPU_temp()
