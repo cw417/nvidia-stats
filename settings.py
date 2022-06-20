88dab7c51683fc5efaa3bb124a97bb539d95e373
cw417 <chriswhitlock417@gmail.com>
2022-06-20 16:07:29 -0400

    Fix error where time needed to be int
    The make_chart file would not run because time in milliseconds was not an integer.

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ start_time = 2 # time to wait before program starts
 
 update_time = 0.1 # time between data queries
 
-update_time_ms = update_time * 100 # update_time in milliseconds
+update_time_ms = int(update_time * 100) # update_time in milliseconds
