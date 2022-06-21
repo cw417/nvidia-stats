2c49168a8dd632524a19514965bd46f67a483943
cw417 <chriswhitlock417@gmail.com>
2022-06-20 21:39:45 -0400

    Break graphs up into 4 separate ones

--- graph.py
+++ graph.py
@@ -12,15 +12,15 @@ def max_clock():
     cmd = 'nvidia-smi -q | grep Video'
     check = str(subprocess.check_output(cmd, shell=True))
     reg = re.findall("\d+", check)
-    return reg[1]
+    return int(reg[1])
 
-max_clock = int(max_clock())
+max_clock = max_clock()
 
 def main():
 
   style.use('seaborn')
 
-  fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
+  fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1, sharex=True)
 
   def animate(i):
 
@@ -32,30 +32,37 @@ def main():
     gpu_use = data['gpu_use']
     fan_speed = data['fan_speed']
 
-    # Define plot 1
+    # temp
     ax1.cla()
     ax1.set_ylim([0,100])
     ax1.plot(time, gpu_temp, label='GPU Temp', color='red')
-    ax1.plot(time, gpu_use, label='GPU Use', color='green', linestyle='--')
-    ax1.plot(time, fan_speed, label='Fan Speed', color='blue', linestyle='--')
+    ax1.legend(loc='upper left')
+    ax1.set_title('GPU Stats') # displays as graph title since this graph is at the top
+    ax1.text(time.iloc[-1], 100, f'GPU Temp: {gpu_temp.iloc[-1]}\u00b0', horizontalalignment='right', verticalalignment='bottom')
 
-    # Define plot 2
+    # clock
     ax2.cla()
     ax2.set_ylim([0,max_clock])
-    ax2.plot(time, gpu_clock, label='GPU Clock')
-
-    # Set plot 1 labels
-    ax1.legend(loc='upper left')
-    ax1.set_title('GPU Stats')
-    ax1.text(time.iloc[-1], 100, f'GPU Temp: {gpu_temp.iloc[-1]}\u00b0', horizontalalignment='right', verticalalignment='bottom')
-    ax1.text(time.iloc[-1], 0, f'GPU Use: {gpu_use.iloc[-1]} \u0025', horizontalalignment='right', verticalalignment='top')
-
-    # Set plot 2 labels
+    ax2.plot(time, gpu_clock, label='GPU Clock', color='blue')
     ax2.legend(loc='upper left')
-    ax2.set_xlabel('Time')
     ax2.text(time.iloc[-1], max_clock, f'GPU Clock: {gpu_clock.iloc[-1]} Mhz', horizontalalignment='right', verticalalignment='bottom')
 
-    plt.tight_layout()
+    # use
+    ax3.cla()
+    ax3.set_ylim([0,100])
+    ax3.plot(time, gpu_use, label='GPU Use', color='green')
+    ax3.legend(loc='upper left')
+    ax3.text(time.iloc[-1], 100, f'GPU Use: {gpu_use.iloc[-1]}%', horizontalalignment='right', verticalalignment='bottom')
+
+    # fan
+    ax4.cla()
+    ax4.set_ylim([0,100])
+    ax4.plot(time, fan_speed, label='Fan Speed', color='black')
+    ax4.legend(loc='upper left')
+    ax4.set_xlabel('Time')
+    ax4.text(time.iloc[-1], 100, f'Fan speed: {fan_speed.iloc[-1]}%', horizontalalignment='right', verticalalignment='bottom')
+
+    #plt.tight_layout()
 
   ani = FuncAnimation(fig, animate, interval=settings.update_time_ms)
 
