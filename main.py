9069cba8cc48e92d6bb5af21cee354ef1e39a52b
cw417 <chriswhitlock417@gmail.com>
2022-06-20 21:17:23 -0400

    Rename files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import stats
-import make_graph
-import make_chart
+import graph
+import chart
 import threading
 import time
 import settings
@@ -8,8 +8,8 @@ import settings
 def main():
     stats_thread = threading.Thread(target=stats.main).start()
     time.sleep(settings.start_time) # sleep is needed to give app time to write to csv so make_graph can have iloc for setting ax#.text variable
-    graph_thread = threading.Thread(target=make_graph.main).start()
-    chart_thread = threading.Thread(target=make_chart.main).start()
+    graph_thread = threading.Thread(target=graph.main).start()
+    chart_thread = threading.Thread(target=chart.main).start()
 
 if __name__ == "__main__":
     main()
